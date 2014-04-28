6208c11c3b79150cde3529168b67ef6ed3720f59
rexthuhking <rajaraaman.m@gmail.com>
2014-04-28 11:24:04 -0700

    Initial check in of MediaUtils
    Initial check in to share with a Stackoverflow user.

---
+++ MediaUtils/OutputPackage.cs
@@ -0,0 +1,13 @@
+﻿using System.Drawing;
+using System.IO;
+
+namespace MediaUtils
+{
+    public class OutputPackage
+    {
+        public MemoryStream VideoStream { get; set; }
+        public Image PreviewImage { get; set; }
+        public string RawOutput { get; set; }
+        public bool Success { get; set; }
+    }
+}
---
+++ MediaUtils/VideoFile.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+
+namespace MediaUtils
+{
+    public class VideoFile
+    {
+
+        private string _Path;
+        public string Path
+        {
+            get
+            {
+                return _Path;
+            }
+            set
+            {
+                _Path = value;
+            }
+        }
+
+        public TimeSpan Duration { get; set; }
+        public double BitRate { get; set; }
+        public string AudioFormat { get; set; }
+        public string VideoFormat { get; set; }
+        public int Height { get; set; }
+        public int Width { get; set; }
+        public string RawInfo { get; set; }
+        public bool infoGathered { get; set; }
+
+
+
+        public VideoFile(string path)
+        {
+            _Path = path;
+            Initialize();
+        }
+
+
+
+        private void Initialize()
+        {
+            this.infoGathered = false;
+            //first make sure we have a value for the video file setting
+            if (string.IsNullOrEmpty(_Path))
+            {
+                throw new Exception("Could not find the location of the video file");
+            }
+
+            //Now see if the video file exists
+            if (!File.Exists(_Path))
+            {
+                throw new Exception("The video file " + _Path + " does not exist.");
+            }
+        }
+
+    }
+}
